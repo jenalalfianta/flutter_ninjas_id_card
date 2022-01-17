cc4f7a194744166f00fd4ea1354eb6c2447f422a
jenalalfianta <jenalalfianta@gmail.com>
2022-01-17 20:43:30 +0700

    penambahan floating button dan belajar StatefulWidget

--- lib/main.dart
+++ lib/main.dart
@@ -4,8 +4,15 @@ void main() => runApp(MaterialApp(
       home: NinjaCard(),
     ));
 
-class NinjaCard extends StatelessWidget {
+class NinjaCard extends StatefulWidget {
   const NinjaCard({Key? key}) : super(key: key);
+
+  @override
+  State<NinjaCard> createState() => _NinjaCardState();
+}
+
+class _NinjaCardState extends State<NinjaCard> {
+  int ninjaLevel = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -16,6 +23,15 @@ class NinjaCard extends StatelessWidget {
           centerTitle: true,
           backgroundColor: Colors.grey[850],
           elevation: 0.0,
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            setState(() {
+              ninjaLevel += 1;
+            });
+          },
+          child: Icon(Icons.add),
+          backgroundColor: Colors.grey.shade800,
         ),
         body: Padding(
           padding: EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0.0),
@@ -59,7 +75,7 @@ class NinjaCard extends StatelessWidget {
               ),
               SizedBox(height: 10.0),
               Text(
-                'ADVANCE',
+                '$ninjaLevel',
                 style: TextStyle(
                   color: Colors.amberAccent[200],
                   letterSpacing: 2.0,
